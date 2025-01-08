7b349d44d4739d3173ee63761b1c67010ecf8b79
ValentinDBS <pandarcha60@gmail.com>
2025-01-08 18:04:16 +0100

    fixing problem with one animation

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,17 +1,29 @@
-'use client';
+"use client";
 
-import { useState } from 'react'
-import Link from 'next/link'
-import Searchbar from '../Searchbar'
-import Image from 'next/image'
-import { IoMdArrowDropdown } from "react-icons/io"
-import Nav from './Nav/Nav';
-import { motion, AnimatePresence } from 'framer-motion'
-
+import { useGSAP } from "@gsap/react";
+import { AnimatePresence } from "framer-motion";
+import gsap from "gsap";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import Searchbar from "../Searchbar";
+import Nav from "./Nav/Nav";
 
 function Header() {
   const [isActive, setIsActive] = useState(false);
 
+  useGSAP(() => {
+    const tl = gsap.timeline({ defaults: { duration: 1, ease: "power1" } });
+
+    tl.from(".header-container > header", {
+      yPercent: -200,
+    });
+
+    tl.from(".hero-container > span", {
+      yPercent: 100,
+      opacity: 0,
+    });
+  }, []);
   return (
     <div className="header-container">
       <header>
@@ -24,32 +36,31 @@ function Header() {
                     src="/images/valorant-logo.webp"
                     width={50}
                     height={50}
-                    alt="Picture of the author" />
+                    alt="Picture of the author"
+                  />
                 </Link>
               </li>
               <li>
-                <Link href="/#agents">
-                  Agents
-                </Link>
+                <Link href="/#agents">Agents</Link>
               </li>
             </div>
             <Searchbar />
           </ul>
         </nav>
-        
       </header>
-      <div onClick={() => { setIsActive(!isActive); } } className='btn-menu'>
-          <div className={`${"burger"} ${isActive ? "burgerActive" : ""}`}>
-          </div>
-        </div>
-        <AnimatePresence mode="wait">
-
-          {isActive && <Nav setIsActive={setIsActive}/>}
-
-        </AnimatePresence>
+      <div
+        onClick={() => {
+          setIsActive(!isActive);
+        }}
+        className="btn-menu"
+      >
+        <div className={`${"burger"} ${isActive ? "burgerActive" : ""}`}></div>
+      </div>
+      <AnimatePresence mode="wait">
+        {isActive && <Nav setIsActive={setIsActive} />}
+      </AnimatePresence>
     </div>
-    
   );
 }
 
-export default Header
+export default Header;
